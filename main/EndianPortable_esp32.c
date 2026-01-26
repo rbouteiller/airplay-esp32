5122e3333e82a5b29986d24d601e7441034e10d5
Rémi Bouteiller <bouteiller.remi@gmail.com>
2026-01-26 10:34:34 +0100

    Better audio codec

--- main/EndianPortable_esp32.c
+++
@@ -1,9 +0,0 @@
-/*
- * Wrapper for EndianPortable.c that ensures proper endianness detection on ESP32
- * ESP32 is little-endian, so we need to make sure the swap functions work correctly
- */
-
-#include <endian.h>  // This defines __BYTE_ORDER and __LITTLE_ENDIAN on ESP-IDF
-
-// Now include the original implementation
-#include "../external/alac/codec/EndianPortable.c"
