711f05a5e9b982e6b16da8a380478cb44710c669
Emlyn Bolton <3941071+emlynmac@users.noreply.github.com>
2026-02-08 13:36:48 -0800

    Remove SPDIF from SqueezeAmp board file

--- main/audio/squeezeamp.c
+++ main/audio/squeezeamp.c
@@ -59,7 +59,6 @@ static TaskHandle_t gpio_task_handle = NULL;
 static volatile bool speaker_fault_active = false;
 static volatile bool headphone_inserted = false;
 
-static esp_err_t init_spdif_gpio(void);
 static esp_err_t init_mute_gpio(void);
 static esp_err_t init_spkfault_gpio(void);
 static esp_err_t init_jack_gpio(void);
@@ -177,12 +176,6 @@ esp_err_t squeezeamp_init(void) {
     return err;
   }
 
-  // Enable GPIO for SPDIF output (WIP)
-  err = init_spdif_gpio();
-  if (err != ESP_OK) {
-    return err;
-  }
-
   // Configure mute GPIO
   err = init_mute_gpio();
   if (err != ESP_OK) {
@@ -208,22 +201,6 @@ esp_err_t squeezeamp_init(void) {
   tas57xx_set_power_mode(TAS57XX_AMP_OFF);
 
   ESP_LOGI(TAG, "SqueezeAMP DAC initialized");
-  return ESP_OK;
-}
-
-static esp_err_t init_spdif_gpio(void) {
-#if CONFIG_SPDIF_DO_IO >= 0
-  gpio_config_t spdif_gpio_cfg = {
-      .pin_bit_mask = (1ULL << CONFIG_SPDIF_DO_IO),
-      .mode = GPIO_MODE_OUTPUT,
-      .pull_up_en = GPIO_PULLUP_DISABLE,
-      .pull_down_en = GPIO_PULLDOWN_DISABLE,
-      .intr_type = GPIO_INTR_DISABLE,
-  };
-  esp_err_t err = gpio_config(&spdif_gpio_cfg);
-  ESP_RETURN_ON_ERROR(err, TAG, "Failed to configure SPDIF GPIO");
-  gpio_set_level(CONFIG_SPDIF_DO_IO, 0);
-#endif
   return ESP_OK;
 }
 
